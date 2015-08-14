c8ef57d79e4a4396663948083b9e3b11a3e22090
Shane Bauer <shane@shanebauer.com>
2015-08-14 00:05:19 -0400

    Added email cases

---
+++ src/MongoIdentity.IntegrationTests/UserEmailTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using MongoIdentity.TestHelpers;
+using NUnit.Framework;
+using Shouldly;
+
+namespace MongoIdentity.IntegrationTests
+{
+    public class UserEmailTests: IntegrationTest
+    {
+        [Test]
+        public void CanGetUserEmail()
+        {
+            var user = IdentityUserMother.BasicUser();
+            UserManager.Create(user);
+
+            var email = UserManager.GetEmail(user.Id);
+
+            email.ShouldBe(user.EmailAddress);
+        }
+
+        [Test]
+        public void CanSetUnconfirmedEmail()
+        {
+            var user = IdentityUserMother.BasicUser();
+            UserManager.Create(user);
+
+            var result = UserManager.SetEmail(user.Id,"new@testing.com");
+
+            var newUser = UserManager.FindById(user.Id);
+
+            result.Succeeded.ShouldBe(true);
+            newUser.EmailAddress.ShouldBe("new@testing.com");
+            newUser.EmailConfirmed.ShouldBe(false);
+        }
+
+        [Test]
+        public void CanConfirmEmailAddress()
+        {
+            var user = IdentityUserMother.BasicUser();
+            UserManager.Create(user);
+
+            var token = UserManager.GenerateEmailConfirmationToken(user.Id);
+
+            token.ShouldNotBeEmpty();
+
+            var result = UserManager.ConfirmEmail(user.Id, token);
+
+            result.Succeeded.ShouldBe(true);
+
+            var newUser = UserManager.FindById(user.Id);
+
+            newUser.EmailConfirmed.ShouldBe(true);
+        }
+
+        [Test]
+        public void CanGetEmailConfirmed()
+        {
+            var user = IdentityUserMother.BasicUser();
+            UserManager.Create(user);
+
+            var token = UserManager.GenerateEmailConfirmationToken(user.Id);
+            var result = UserManager.ConfirmEmail(user.Id, token);
+
+            result.Succeeded.ShouldBe(true);
+
+            UserManager.IsEmailConfirmed(user.Id).ShouldBe(true);
+        }
+    }
+}
